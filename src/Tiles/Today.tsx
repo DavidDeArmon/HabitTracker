054aba60c8985edaf6e3330d175d11bb60401957
David DeArmon <david.dearmon@gmail.com>
2022-12-02 19:38:26 -0500

    MVP for habit tracking using firestore.

--- src/Tiles/Today.tsx
+++ src/Tiles/Today.tsx
@@ -1,84 +1,101 @@
 import { User } from "firebase/auth";
-import { collection, addDoc, getDocs, query, where, orderBy, limit, Timestamp } from "firebase/firestore";
-import { useEffect, useState, forwardRef, createRef } from "react";
+import { collection, addDoc, setDoc, doc, getDocs, query, where, limit, Timestamp} from "firebase/firestore";
+import { useEffect, useState, forwardRef } from "react";
 import { fireDB } from '../firebaseConfig'
-import DatePicker, { ReactDatePicker, ReactDatePickerProps } from "react-datepicker";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+
 interface myProps {
     user: User | null
     isVerified: boolean
 }
 
+
 function Today(props: React.PropsWithChildren<myProps>) {
     const [savedMood, setMood] = useState('n/a')
-    const [selectedDate, setSelectedDate] = useState(new Date)
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const [selectedMoodDoc, setSelectedMoodDoc] = useState("")
 
     useEffect(() => {
         GetMood()
-        return setSelectedDate(new Date)
-    }, [savedMood])
-
-    async function setDate(newDate: Date) {
-        setSelectedDate(newDate)
-        GetMood()
-    }
+    })
 
     async function AddMood(newMood: string) {
-        if (props.user) try {
-            const docRef = await addDoc(collection(fireDB, "mood"), {
-                date: Timestamp.now(),
+        if (props.isVerified && props.user) try {
+            let writeData = {
+                date: Timestamp.fromDate(selectedDate),
                 mood: newMood,
                 uid: props.user.uid,
-            });
-            setMood(newMood)
-            console.log("Document written with ID: ", docRef.id, newMood);
+            }
+            if(selectedMoodDoc){
+                let documentReference = doc(fireDB, "users", props.user.uid, "moods", selectedMoodDoc)
+                await setDoc(documentReference, writeData)
+            }else{
+                let userMoodCollection = collection(fireDB, "users", props.user.uid, "moods")
+                let moodDocument = await addDoc(userMoodCollection, writeData);
+                setSelectedMoodDoc(moodDocument.id);
+            }
+
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     }
+
     async function GetMood() {
-        if (props.isVerified) try {
-            const newQuery = query(collection(fireDB, "mood"), where("uid", "==", props.user?.uid), orderBy("date", "desc"), limit(1));
-            const moodQuerySnapshot = await getDocs(newQuery);
-            moodQuerySnapshot.forEach((entry) => {
-                let doc = entry.data()
-                let today = selectedDate
-                today.setHours(0, 0, 0, 0)
-                console.log(doc)
-                let isTodaysMood = (doc['date'] >= Timestamp.fromDate(today))
-                console.log(isTodaysMood + " docDate: " + doc['date'] + "today:" + Timestamp.fromDate(today))
-                if (isTodaysMood) { setMood(doc['mood']) }
-            })
+        if (props.isVerified && props.user) try {
+
+            let queryDate = selectedDate
+            queryDate.setHours(0, 0, 0, 0)
+            let startOfSelectedDate = Timestamp.fromDate(queryDate)
+            queryDate.setHours(23, 59, 59, 0)
+            let endOfSelectedDate = Timestamp.fromDate(queryDate)
+
+            const queryGetUsersMoods = query(collection(fireDB, "users", props.user.uid, "moods"), where("date", ">=", startOfSelectedDate), where("date", "<=", endOfSelectedDate), limit(1));
+            await getDocs(queryGetUsersMoods).then((results) => {
+                let newMood = "n/a";
+                results.forEach((entry) => {
+                    let doc = entry.data();
+                    if (doc && doc['mood']) {
+                        newMood = doc['mood'];
+                        setSelectedMoodDoc(doc['ref']);
+                    }
+                })
+                setMood(newMood);
+            });
+
         } catch (e) {
             console.error("Error reading document: ", e);
+            setMood('n/a')
         }
         return savedMood
     }
 
-    function moodSelecter() {
+    function MoodSelecter() {
         const possibleMoods = ["Great", "Good", "Okay", "Bad"]
-        let moodSelecter = possibleMoods.map((e, i) => {
+        let moodButtons = possibleMoods.map((e, i) => {
             let disableButton = (e === savedMood);
             return <button className="moodOption" disabled={disableButton} key={i} onClick={() => AddMood(e)}>{e}</button>
         })
-        return moodSelecter;
+        return moodButtons;
     }
+
     const ExampleCustomInput = forwardRef<HTMLButtonElement, any>(({ value, onClick }, ref) => (
         <>
             <button className="dateSelectionButton" ref={ref} onClick={onClick}>{value}</button>
         </>
     ));
+
+
     return (
         <div className="today">
-            <DatePicker selected={selectedDate} onChange={(dt: Date) => setDate(dt)} customInput={<ExampleCustomInput />} />
+            <DatePicker selected={selectedDate} onChange={(date: Date) => setSelectedDate(date)} customInput={<ExampleCustomInput />} />
             <p>
                 <b>What's your mood today?</b>
                 <br /><small>{savedMood}</small>
             </p>
-
             <div>
-                <button >Add Mood</button>
-                {moodSelecter()}
+                {MoodSelecter()}
             </div>
         </div>
     );
